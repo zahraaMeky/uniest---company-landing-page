5f593ca1517375c510656c28fc40fe36612d77e4
zahraa Meky <123356306+zahraaMeky@users.noreply.github.com>
2025-03-26 23:19:50 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
         <Link href='/'>
             <Image src="/uniest-logo.svg" alt="uniest logo" width={185} height={56}></Image>    
         </Link>
-        <ul className="hidden lg:flex h-full gap-12 bg-[#808080] rounded-full px-8 py-2">
+        <ul className="hidden lg:flex h-full gap-12 bg-[#808080] rounded-full px-8 py-4">
             {NAV_LINKS.map((link) => (
             <Link href={link.href} key={link.key} className="regular-16 text-white flexCenter cursor-pointer transition-all">
                 {link.label}
@@ -23,7 +23,7 @@ export const Navbar = () => {
           icon="/getStartBtnIcon.svg"
           variant="bg-gradient"
           textColor="text-white"
-          className="px-8 py-2 border-0 rounded-2xl"
+          className="px-8 py-4 border-0 rounded-2xl"
         />
       </div>
     </nav>
