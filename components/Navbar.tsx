995d66dcf7876ece6f3a964a0c97901470b47032
zahraa Meky <123356306+zahraaMeky@users.noreply.github.com>
2025-04-08 23:56:10 +0200

    Update Navbar.tsx
    make active class

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,48 @@
+'use client'
+
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 import Button from "./Button"
+import { usePathname } from 'next/navigation'
 
 export const Navbar = () => {
+  const pathName = usePathname()
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-        <Link href='/'>
-            <Image src="/uniest-logo.svg" alt="uniest logo" width={185} height={56}></Image>    
-        </Link>
-        <ul className="hidden lg:flex h-full gap-12 bg-[#808080] rounded-full px-8 py-4">
-            {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key} className="regular-16 text-white flexCenter cursor-pointer transition-all">
-                {link.label}
+      <Link href='/'>
+        <Image src="/uniest-logo.svg" alt="uniest logo" width={185} height={56} />
+      </Link>
+
+      <ul className="hidden lg:flex h-full gap-12 bg-[#808080] rounded-full px-4 py-2">
+        {NAV_LINKS.map((link) => {
+          const isActive = pathName === link.href;
+          return (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 flexCenter cursor-pointer transition-all"
+            >
+              <span className={`px-4 py-2 rounded-full transition-all ${
+                isActive
+                  ? 'bg-white'
+                  : 'text-white hover:text-primary'
+              }`}>
+                <span className={`${
+                  isActive
+                    ? 'bg-gradient-to-r from-[#E3058A] to-[#0A6195] text-transparent bg-clip-text font-semibold'
+                    : ''
+                }`}>
+                  {link.label}
+                </span>
+              </span>
             </Link>
-            ))}
-        </ul>
-        <div className="hidden lg:flexCenter">
+          )
+        })}
+      </ul>
+
+      <div className="hidden lg:flexCenter">
         <Button 
           type="button"
           title="Get Started"
