dc2ccbc6f0ae23a233e21f7d1a1db43cd2693839
zahraa Meky <123356306+zahraaMeky@users.noreply.github.com>
2025-04-29 01:39:10 +0300

    Update Ecosystem Component

--- components/EcosystemCard.tsx
+++ components/EcosystemCard.tsx
@@ -9,32 +9,24 @@ type EcosystemCardProps = {
 const EcosystemCard = ({ title, icon, description }: EcosystemCardProps) => {
   return (
     <li 
-      className="relative flex flex-col items-center justify-center border-t-4 border-[#93278F] px-6 py-6 cursor-pointer rounded-lg shadow-lg bg-white transition-transform duration-300 hover:scale-105"
+      className="relative flex flex-col items-center justify-start pt-15 pb-8 px-6 cursor-pointer rounded-lg shadow-lg bg-white border-t-4 border-[#93278F] transition-transform duration-300 hover:scale-105 mb-15 md:mb-0"
     >
-      {/* Icon with Background */}
-      <div className="relative w-full flex items-center justify-center h-[63px] mb-4">
-        {/* Background Image */}
-        <Image
-          src="/cardbg.svg"
-          alt="card background"
-          width={189}
-          height={63}
-          className="absolute top-0 left-1/2 -translate-x-1/2"
-        />
-
-        {/* Icon */}
-        <div className="relative z-10 w-14 h-14 flex items-center justify-center">
-          <Image src={icon} alt={title} width={48} height={36} />
-        </div>
-      </div>
+      {/* Background Image at top */}
+      <Image
+        src={icon}
+        alt="Card Background"
+        width={189}
+        height={63}
+        className="absolute -top-[21px] left-1/2 -translate-x-1/2 z-0" 
+      />
 
       {/* Title */}
-      <h2 className="text-xl font-bold mt-2 capitalize text-[#18273A]">
+      <h2 className="text-xl font-bold capitalize text-[#18273A] text-center">
         {title}
       </h2>
 
       {/* Description */}
-      <p className="regular-16 text-[#5D5D5D] mt-3 text-center">
+      <p className="text-base text-[#5D5D5D] mt-3 text-center">
         {description}
       </p>
     </li>
